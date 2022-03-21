5efece8bc7876a288197c9fc005f050d6bf43499
Nhat Minh <95019041+walacker@users.noreply.github.com>
2022-03-21 21:22:04 +0700

    Update EP05006.cpp

--- code/EP05006.cpp
+++ code/EP05006.cpp
@@ -13,8 +13,8 @@
 #define sz(a) int((a).size())
 #define pb push_back
 #define pf push_front
-#define fi first
-#define se second
+#define f first
+#define s second
 #define mp make_pair
 #define nc(x) cout << (x) << '\n';
 #define faster() ios_base::sync_with_stdio(0); cin.tie(NULL);cout.tie(NULL);
@@ -33,39 +33,47 @@ inline ll lcm(ll a,ll b){return a/gcd(a,b)*b;}
 
 using namespace std;
 
+void io(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+bool cmp(pair<string, int> a, pair<string, int> b) {
+    if (a.s == b.s) return a.f < b.f; //nếu bằng số lần xuất hiện thì in theo bảng chữ cái
+    return a.s > b.s; //in số lần xuất hiện
+}
+
 int main() {
-    faster();
-    //#ifndef ONLINE_JUDGE
-    //freopen("DOCUMENT.in", "r", stdin);
-    //freopen("output.txt", "w", stdout);
-    //#endif
-    int n ,max = 0; 
-	cin >> n ; 
-	string s  ; 
-	set<string> x ;
-	map<string ,int > mp ; 
-	cin.ignore(); 
-	FOR(i,0,n){
-		getline(cin,s) ; 
-		int res = sz(s);
-		FOR(i,0,res){
-			s[i] = tolower(s[i]);
-			if(s[i]==','||s[i]=='.'||s[i]=='?'||s[i]=='!'||s[i]==':'||s[i]==';'||s[i]=='-'||s[i]=='/') s[i] = ' ' ; 		
-		}
-		stringstream ss(s) ; 
-		string token ; 
-		while(ss>>token){
-			if(mp[token]==0) x.ins(token);
-			mp[token]++;
-			if(mp[token]>max) max = mp[token];
-		}
-		//cout << s << endl ; 
-	}
-	while(max>=1){
-		for( string s : x ) {
-			if(mp[s]==max) cout << s << " " << mp[s] << endl;
-		}
-		max--;
-	}
+   faster();
+   //io();
+   //freopen("VANBAN.in", "r", stdin);
+   int n;
+   cin >> n;
+   vector<string> list;
+   string delimit = ",.?!:;-/)('_\"–";
+   map<string, int> danhdau;
+   while (n--) {
+       string s;
+       getline(cin >> ws, s);
+       for (int i = 0; i < s.size(); i++) s[i] = tolower(s[i]);
+        for (int i = 0; i < delimit.size(); i++) {
+            replace(s.begin(), s.end(), delimit[i], ' ');
+        }
+        string news = s;
+        stringstream ss(news);
+        string token;
+        while (ss >> token) {
+            danhdau[token]++;
+        }
+   }
+    vector<pair<string, int> > vectormap;
+    for (auto& it : danhdau) {
+        vectormap.push_back(it);
+    }
+    sort(all(vectormap), cmp);
+    for (auto i : vectormap) {
+         cout << i.f << " " << i.s << endl;
+    }
     return 0 ;
 }
